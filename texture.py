4be7ae114a738d25e481f5b033eee1bec45404e6
unknown <danya.masyanov@yandex.ru>
2023-12-09 12:21:22 +0400

    Modified file texture.py
    Added the documentation for function load_texture(texture_path) and the comments in code

--- texture.py
+++ texture.py
@@ -4,10 +4,18 @@ import numpy as np
 
 
 def load_texture(texture_path):
+    """
+    Загружает текстуру из файла изображения с использованием Pygame.
+
+    Параметры:
+    - texture_path (str): Путь к файлу изображения.
+    """
     texture_surface = pygame.image.load(texture_path)
+    # Преобразование данных текстуры в строковый формат
     texture_data = pygame.image.tostring(texture_surface, "RGBA", 1)
     width, height = texture_surface.get_width(), texture_surface.get_height()
 
+    # Создание и настройка текстуры OpenGL
     texture = glGenTextures(1)
     glBindTexture(GL_TEXTURE_2D, texture)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
@@ -17,12 +25,13 @@ def load_texture(texture_path):
     return texture, width, height
 
 
+# Инициализация Pygame и OpenGL
 pygame.init()
 window_width, window_height = 800, 600
 pygame.display.set_mode((window_width, window_height), pygame.OPENGL | pygame.DOUBLEBUF)
 
+# Настройка текстуры
 texture_path = 'gif&img/tekstura.png'
-# Загрузка текстуры
 texture, texture_width, texture_height = load_texture(texture_path)
 
 # Установка параметров OpenGL
@@ -31,6 +40,7 @@ glClearColor(0.0, 0.0, 0.0, 1.0)
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
+# Инициализация часов Pygame
 clock = pygame.time.Clock()
 
 cursor_x, cursor_y = 0, 0
@@ -42,17 +52,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # Получение координат мыши и их преобразование в систему координат OpenGL
             mouse_x, mouse_y = event.pos
             cursor_x = mouse_x / window_width * 2 - 1
             cursor_y = 1 - mouse_y / window_height * 2
             is_texture_drawn = True
 
-    # Очистка экрана цветом белого
+    # Очистка экрана белым цветом
     glClearColor(1.0, 1.0, 1.0, 1.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
+    # Загрузка единичной матрицы
     glLoadIdentity()
 
+    # Отрисовка текстуры при необходимости
     if is_texture_drawn:
         glBegin(GL_QUADS)
         glTexCoord2f(0, 0)
@@ -65,6 +77,7 @@ while running:
         glVertex2f(-1, cursor_y)
         glEnd()
 
+    # Смена буферов отображения
     pygame.display.flip()
     clock.tick(60)
 
